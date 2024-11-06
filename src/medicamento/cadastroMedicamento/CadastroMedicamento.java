787cf01948ac699eab9f5a8e255c1f50e228b89e
Bruno Nakahara Hirata <37190846+bruno-n-hirata@users.noreply.github.com>
2024-11-06 19:23:20 -0300

    Add files via upload

---
+++ src/medicamento/cadastroMedicamento/CadastroMedicamento.java
@@ -0,0 +1,11 @@
+package medicamento.cadastroMedicamento;
+
+import medicamento.apresentacao.Principal;
+
+public class CadastroMedicamento {
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+}
